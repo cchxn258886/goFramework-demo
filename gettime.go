e7fce75dcb01bfe0c080fb2a7800721faea7e808
chenl <244776516@qq.com>
2021-05-14 08:59:52 +0800

    删除一些不需要的代码

--- gettime.go
+++ gettime.go
@@ -5,25 +5,26 @@ import (
 	"net/http"
 )
 
-func main(){
+//测试
+func main() {
 	//done := make(chan error,2);
 	//stop := make(chan struct{})
 	//go func() {
 	//	done <- server()
 	//}()
-var s = "";
-println(len(s))
-if s == ""{
-	println("true")
+	var s = ""
+	println(len(s))
+	if s == "" {
+		println("true")
+	}
 }
-}
-func server(addr string,handle http.Handler,stop <- chan struct{}) error{
+func server(addr string, handle http.Handler, stop <-chan struct{}) error {
 	s := http.Server{
-		Addr: addr,
+		Addr:    addr,
 		Handler: handle,
 	}
 	go func() {
-		<- stop
+		<-stop
 		s.Shutdown(context.Background())
 	}()
 	return s.ListenAndServe()
